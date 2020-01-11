eb2149e542ae452a642b015b8a76eff2743ee067
richard <richard@mars.rac.local>
2020-01-11 17:35:01 -0500

    Trying simple shading

--- cones.cpp
+++ cones.cpp
@@ -8,9 +8,16 @@
 #define GL_GLEXT_PROTOTYPES
 #include <GL/glut.h>
 #include <glm/glm.hpp>  
-#include "glm/gtc/matrix_transform.hpp"
+#include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include "shader.h"
+
+#define SHOW_HINTS
+
+constexpr float SCREEN_WIDTH = 15.f ;
+constexpr float SCREEN_HEIGHT = 15.f ;
+constexpr float SCREEN_DEPTH = 15.f ;
 // ----------------------------------------------------------
 // Function Prototypes
 // ----------------------------------------------------------
@@ -23,35 +30,115 @@ float rotate_y=0;
 float rotate_x=0;
 float rotate_z=0;
 
+float tmpx = 0.00001 ;
+float tmpy = 0 ;
+
+// ----------------------------------------------------------
+// Circle data
+// ----------------------------------------------------------
+constexpr int BASE_N = 30 ;
+constexpr int SIDES_N = 3 ;
+constexpr double circle_fraction = 0.3333333 ;
+constexpr double side_fan_angle_step = circle_fraction * 2.0 * M_PI / SIDES_N ;
+constexpr double side_curl_angle_step = 2.0 * M_PI / SIDES_N ;
+constexpr double base_radius = 2.0 ;
+constexpr double radius = 4.0 ;
+constexpr double cx = -radius ;
+constexpr double cy = 0 ;
+constexpr float base_angle_step = M_PI * 2.0 / BASE_N ;
+
 // ----------------------------------------------------------
 // Initial foldup rotation
 // ----------------------------------------------------------
-float rotate_fold=0.f ;
+float rotate_fold=0.f ;  // rotation is 0..1.0  (scale later)  
 
-float fold_delta = rotate_fold <= 0.005f ? 0.005f : -0.0005f ;
-constexpr int PAUSE_LEN = 600 ;
-int pause_clock = PAUSE_LEN ;
+float fold_delta = -0.005f ;
+bool pause_clock = false ;
+bool freeze = false ;
+
+// Handle rotating the cone side curl rate
+void fold_rotate() {
+    rotate_fold += fold_delta ;
+    if( rotate_fold >= 1.f ) {
+      rotate_fold = 1.f ;
+      fold_delta = -0.005f ;
+    }
+    if( rotate_fold <= 0.f ) {
+      rotate_fold = 0.f ;
+      fold_delta = 0.005f ;
+    }
+}
+
+// What to do in case of key press
+void keypress_func( unsigned char key, int x, int y ) {
+  if( key == 'x' ) {
+    pause_clock = !pause_clock ;
+  }
+  if( key == 'f' ) {
+    freeze = !freeze ;
+  }
+  if( key == 'w' ) {
+    tmpy += 0.01 ;
+    std::cout << tmpx << "," << tmpy << std::endl ;
+  }
+  if( key == 's' ) {
+    tmpy -= 0.01 ;
+    std::cout << tmpx << "," << tmpy << std::endl ;
+  }
+  if( key == 'a' ) {
+    tmpx -= 0.01 ;
+    std::cout << tmpx << "," << tmpy << std::endl ;
+  }
+  if( key == 'd' ) {
+    tmpx += 0.01 ;
+    std::cout << tmpx << "," << tmpy << std::endl ;
+  }
+
+  if( key == '1' ) {
+    rotate_y=0 ; 
+    rotate_x=0 ;
+    rotate_z=0 ;
+  }
+  if( key == '2' ) {
+    rotate_y=0 ; 
+    rotate_x=M_PI_4 ;
+    rotate_z=0 ;
+  }
+  if( key == '3' ) {
+    rotate_y=M_PI_4 ; 
+    rotate_x=M_PI_4 ;
+    rotate_z=0 ;
+  }
+  if( key == '4' ) {
+    rotate_y=M_PI_4 ; 
+    rotate_x=0 ;
+    rotate_z=0 ;
+  }
+  if( key == 'q' ) {
+    exit( 0 ) ;
+  }
+  if( key == ' ' ) {
+    fold_rotate() ;
+  }
+}
 
 // ----------------------------------------------------------
 // display() Callback function
 // ----------------------------------------------------------
 void display(){
- 
+
+  if( !freeze ) { 
  // Rotate scene
-  rotate_x += 0.01f ;
-  rotate_y += 0.003f ;
-  rotate_z += 0.02f ;
+    rotate_x += 0.002f ;
+    rotate_y += 0.003f ;
+    rotate_z += 0.005f ;
+  }
 // update fold - if at either end of fold
 // pause fold effects temporarily
-  if( pause_clock < 0 ) {
-    rotate_fold += fold_delta ;
-    if( rotate_fold >= (float)M_PI_2 || rotate_fold <= 0.f ) {
-      fold_delta = -fold_delta ;
-      pause_clock = PAUSE_LEN ;
-    }
-  } else {
-    pause_clock-- ;
-  }
+  if( !pause_clock ) {
+    fold_rotate() ;
+  } 
+  
 
   //  Clear screen ard Z-buffer
   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
@@ -71,69 +158,46 @@ void display(){
 
   // -----------------------------
   // B A S E
-  glColor3f(   0.1,  0.1,  0.3 );  
-
   glMultMatrixf( glm::value_ptr(matrixModel) );
-  
-  glColor3f(0.5,0.5,0.2);
+
+  float angle = 0.0 ;
+
+  glColor3f(0.75,0.4,0.2);
   glBegin(GL_TRIANGLE_FAN);
 
-glVertex3f( 0 , 0 , 0.00 );
-glVertex3f( 2.0 , 0.0 , 0.00 );
-glVertex3f( 1.902113032590307 , 0.6180339887498948 , 0.00 );
-glVertex3f( 1.618033988749895 , 1.1755705045849463 , 0.00 );
-glVertex3f( 1.1755705045849463 , 1.618033988749895 , 0.00 );
-glVertex3f( 0.6180339887498949 , 1.902113032590307 , 0.00 );
-glVertex3f( 1.2246467991473532e-16 , 2.0 , 0.00 );
-glVertex3f( -0.6180339887498947 , 1.9021130325903073 , 0.00 );
-glVertex3f( -1.175570504584946 , 1.618033988749895 , 0.00 );
-glVertex3f( -1.6180339887498947 , 1.1755705045849465 , 0.00 );
-glVertex3f( -1.902113032590307 , 0.618033988749895 , 0.00 );
-glVertex3f( -2.0 , 2.4492935982947064e-16 , 0.00 );
-glVertex3f( -1.9021130325903073 , -0.6180339887498946 , 0.00 );
-glVertex3f( -1.6180339887498951 , -1.175570504584946 , 0.00 );
-glVertex3f( -1.1755705045849465 , -1.6180339887498947 , 0.00 );
-glVertex3f( -0.6180339887498951 , -1.902113032590307 , 0.00 );
-glVertex3f( -3.6739403974420594e-16 , -2.0 , 0.00 );
-glVertex3f( 0.6180339887498945 , -1.9021130325903073 , 0.00 );
-glVertex3f( 1.1755705045849458 , -1.6180339887498951 , 0.00 );
-glVertex3f( 1.6180339887498947 , -1.1755705045849467 , 0.00 );
-glVertex3f( 1.902113032590307 , -0.6180339887498952 , 0.00 );
-glVertex3f( 2.0 , -4.898587196589413e-16 , 0.00 );
+  glVertex3f( 0.f , 0.f , 0.00 );  // Centre
 
+  // define triangle base points
+  for( int i=0 ; i<=BASE_N ; i++ ) {
+    float x = base_radius * std::cos( angle ) ;
+    float y = base_radius * std::sin( angle ) ;
+    angle = angle + base_angle_step ;
+    glVertex3f( x, y, 0.00 ) ;
+  }
   glEnd();
 
   // -----------------------------
   // S I D E S
 
-  glPushMatrix() ;
+  float fan_angle = 0.0 ;
+  float curl_angle = 0.0 ;
 
-  glm::mat4 matrixModelY1 = glm::rotate( glm::mat4(1.0f), rotate_fold, glm::vec3(0.0, 1.0, 0.0) );
-  glm::mat4 matrixModelT1 = glm::translate( glm::mat4(1.0f), glm::vec3( -base_radius, 0.0, 0) );
-  glm::mat4 matrixModel1 = matrixModelT1 * matrixModelY1 ;
-
-  glMultMatrixf( glm::value_ptr(matrixModel1) );
-
-  glColor3f(0.2,0.2,0.2);
+  glColor3f( 0.2, 0.5, 0.5 );
   glBegin(GL_TRIANGLE_FAN);
 
-  glVertex3f( -5 , 0 , 0.00 );
-  glVertex3f( 0.0 , 0.0 , 0.00 );
-  glVertex3f( -0.17037086855465855 , 1.2940952255126037 , 0.00 );
-  glVertex3f( -0.6698729810778064 , 2.4999999999999996 , 0.00 );
-  glVertex3f( -1.4644660940672622 , 3.5355339059327373 , 0.00 );
-  glVertex3f( -2.4999999999999996 , 4.330127018922193 , 0.00 );
-  glVertex3f( -3.7059047744873954 , 4.829629131445341 , 0.00 );
-  glVertex3f( -4.999999999999998 , 5.0 , 0.00 );
-  glVertex3f( -6.294095225512602 , 4.829629131445342 , 0.00 );
-  glVertex3f( -7.499999999999999 , 4.330127018922194 , 0.00 );
-  glVertex3f( -8.535533905932738 , 3.5355339059327378 , 0.00 );
-  glVertex3f( -9.330127018922195 , 2.4999999999999996 , 0.00 );
-  glVertex3f( -9.82962913144534 , 1.2940952255126028 , 0.00 );
-  glVertex3f( -10.0 , -1.6081226496766366e-15 , 0.00 );
+  glVertex3f( 0-radius-base_radius , 0.f , 0.00 );  // Centre
 
+  // define triangle base points
+  for( int i=0 ; i<=SIDES_N ; i++ ) {
+    float x = radius * std::cos( fan_angle / circle_fraction ) ;
+    float y = radius * std::sin( fan_angle / circle_fraction ) ;
+    float z = base_radius * std::cos( curl_angle ) ;
+    fan_angle += side_fan_angle_step ;
+    curl_angle += side_curl_angle_step ;
+    glVertex3f( x-radius-base_radius, y, 5 ) ;
+  }
   glEnd();
-  glPopMatrix() ;
+
 
   glFlush();
   glutSwapBuffers();
@@ -154,24 +218,31 @@ int main(int argc, char* argv[]){
   //  Request double buffered true color window with Z-buffer
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
  
+  Shader theShader("shader.vs", "shader.fs"); // you can name your shader files however you like
+
   // Create window
   glutCreateWindow("Orientation");
   glutReshapeWindow( 400, 400 );
-  
+
+  // Add keypress listener for current window
+  glutKeyboardFunc( keypress_func ) ;
+
   //  Enable Z-buffer depth test
   glEnable(GL_DEPTH_TEST);
 
   glEnable(GL_TEXTURE_2D) ;
 
-  glViewport( 0.f, 0.0f, 20.f, 20.f ); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
+  glViewport( 0.f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT ); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
   glMatrixMode( GL_PROJECTION ); // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
   glLoadIdentity( ); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
-  glOrtho( -10.f, 10.f, -10.f, 10.f, -10.f, 10.f ); // essentially set coordinate system
+  glOrtho( -SCREEN_WIDTH/2.f, SCREEN_WIDTH/2.f, -SCREEN_HEIGHT/2.f, SCREEN_HEIGHT/2.f, -SCREEN_DEPTH/2.f, SCREEN_DEPTH/2.f ); // essentially set coordinate system
   glMatrixMode( GL_MODELVIEW ); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
 
   // Callback functions
   glutDisplayFunc(display);
 
+  theShader.use() ;
+  
   //  Pass control to GLUT for events
   glutMainLoop();
  
